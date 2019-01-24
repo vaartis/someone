e82499a0cb9d95b6159bce0ee00a48e8d6351ce6
Ekaterina Vaartis <vaartis@cock.li>
2019-01-25 00:20:02 +0300

    Make lines in terminal split at the end, should make this better
    Like making it split the next line and the history, etc, too

--- src/term.rs
+++ src/term.rs
@@ -9,6 +9,20 @@ pub struct TerminalState {
     // TODO: history
 }
 
+const FONT_SIZE: u32 = 16;
+
+/// Calculates how many characters of the string could fit into a line of a given width.
+pub fn chars_fit_into(string: &str, width: f32, font: &Font, font_size: u32) -> usize {
+    // TODO: figure out what "outline thickness" is, for now 1.0 seems to do the job fine
+    let current_width = string.chars().fold(0.0, |acc, ch| acc + font.glyph(ch as u32, font_size, false, 1.0).bounds.width);
+
+    if current_width < width {
+        string.len()
+    } else {
+        chars_fit_into(&string[..string.len() - 1], width, font, font_size)
+    }
+}
+
 pub fn draw_terminal(window: &mut RenderWindow, font: &Font, state: &mut TerminalState) {
     let win_size = window.size();
 
@@ -18,6 +32,7 @@ pub fn draw_terminal(window: &mut RenderWindow, font: &Font, state: &mut Termina
     // most of the window
     let rect_height = win_size.y / 100 * (80 - 10);
     let rect_width = win_size.x - (width_offset * 2);
+    let allowed_text_width = rect_width - (width_offset * 2);
 
     let mut rect = RectangleShape::with_size(Vector2f::new(rect_width as f32, rect_height as f32));
     rect.set_outline_thickness(2.0);
@@ -25,10 +40,20 @@ pub fn draw_terminal(window: &mut RenderWindow, font: &Font, state: &mut Termina
     rect.set_fill_color(&Color::BLACK);
     rect.set_position(Vector2f::new(width_offset as f32, height_offset as f32));
 
-    let line_txt = format!("$ {}", &state.line);
-    let txt_to_drw: String = state.whole_text.clone() + &line_txt;
+    // Format the line so it'd include $ in the beginning
+    let line = format!("$ {}", &state.line);
+    let supposed_fit_length = chars_fit_into(&line, allowed_text_width as f32, &font, FONT_SIZE);
 
-    let mut text = Text::new(&txt_to_drw, &font, 16);
+    let new_line = if line.len() > supposed_fit_length {
+        let (before, after) = line.split_at(supposed_fit_length - 1);
+        format!("{}\n{}", before, after)
+    } else {
+        line
+    };
+
+    let txt_to_use = format!("{}{}", &state.whole_text, &new_line);
+    let mut text = Text::new(&txt_to_use, &font, FONT_SIZE);
+
     text.set_fill_color(&Color::WHITE);
     text.set_position(Vector2f::new((width_offset * 2) as f32, (height_offset * 2)  as f32));
 
