660f095f6e2528ae3386467b2780c43c7014eb67
Ekaterina Vaartis <vaartis@cock.li>
2020-04-28 17:22:52 +0300

    Fix formatting in story_parser.cpp

--- src/story_parser.cpp
+++ src/story_parser.cpp
@@ -71,70 +71,113 @@ void StoryParser::maybe_parse_referenced_file(std::string next, lines_type &resu
 }
 
 void StoryParser::parse(lines_type &result, std::string file_name, sol::state &lua) {
-            auto full_file_name = std::filesystem::path("resources/story/") / file_name;
-        full_file_name.replace_extension(".yml");
+    auto full_file_name = std::filesystem::path("resources/story/") / file_name;
+    full_file_name.replace_extension(".yml");
 
-        std::string nmspace = file_name;
+    std::string nmspace = file_name;
 
-        YAML::Node root_node = YAML::LoadFile(full_file_name);
+    YAML::Node root_node = YAML::LoadFile(full_file_name);
 
-        std::map<std::string, CharacterConfig> character_configs;
-        if (root_node["config"]) {
-            auto config = root_node["config"];
+    std::map<std::string, CharacterConfig> character_configs;
+    if (root_node["config"]) {
+        auto config = root_node["config"];
 
-            if (config["chars"]) {
-                character_configs = config["chars"].as<decltype(character_configs)>();
-            }
+        if (config["chars"]) {
+            character_configs = config["chars"].as<decltype(character_configs)>();
+        }
+    }
+
+    // A map of reference to the nodes to check after everything has been built
+    std::map<std::string, std::string> next_references_to_check;
+
+    for (auto name_and_val : root_node) {
+        auto name = name_and_val.first.as<std::string>();
+        auto node = name_and_val.second;
+
+        // Skip the config node
+        if (name == "config") continue;
+
+        auto inserted_name = fmt::format("{}/{}", nmspace, name);
+
+        if (!node["char"]) {
+            spdlog::error("{} doesn't specify a character", inserted_name);
         }
 
-        // A map of reference to the nodes to check after everything has been built
-        std::map<std::string, std::string> next_references_to_check;
+        auto node_char = node["char"].as<std::string>();
 
-        for (auto name_and_val : root_node) {
-            auto name = name_and_val.first.as<std::string>();
-            auto node = name_and_val.second;
+        CharacterConfig char_config;
+        auto found_char_config = character_configs.find(node_char);
+        if (found_char_config == character_configs.end()) {
+            spdlog::warn(
+                "{} uses the character {}, but it does not exist",
+                inserted_name,
+                node_char
+            );
+        } else {
+            char_config = found_char_config->second;
+        }
 
-            // Skip the config node
-            if (name == "config") continue;
+        std::optional<std::string> node_script;
+        if (node["script"]) node_script = node["script"].as<std::string>();
 
-            auto inserted_name = fmt::format("{}/{}", nmspace, name);
+        std::optional<std::string> node_script_after;
+        if (node["script_after"])
+            node_script_after = node["script_after"].as<std::string>();
 
-            if (!node["char"]) {
-                spdlog::error("{} doesn't specify a character", inserted_name);
-            }
 
-            auto node_char = node["char"].as<std::string>();
+        sol::object result_object;
 
-            CharacterConfig char_config;
-            auto found_char_config = character_configs.find(node_char);
-            if (found_char_config == character_configs.end()) {
-                spdlog::warn(
-                    "{} uses the character {}, but it does not exist",
-                    inserted_name,
-                    node_char
-                );
+        if (node["text"]) {
+            // Get the text of the node
+            auto text = node["text"].as<std::string>();
+
+            std::string next;
+            if (node["next"]) {
+                next = node["next"].as<std::string>();
+            } else if (!node["next"] && StringUtils::is_number(name)) {
+                // If there's no next node, but the name of the node is a number,
+                // try using the next number as the next node
+
+                auto next_num = std::stoi(name) + 1;
+                auto maybe_next_name = std::to_string(next_num);
+                auto maybe_next_numbered = root_node[maybe_next_name];
+
+                if (maybe_next_numbered)
+                    // If it exists, mark it as next
+                    next = maybe_next_name;
+                else
+                    // Otherwise log that there was no more numbers, let "" be there
+                    spdlog::warn("Next numbered line not found for {} and there's no 'next'", inserted_name);
             } else {
-                char_config = found_char_config->second;
+                // If there was no next at all and no patterns matched, let "" be there
+                spdlog::warn("No 'next' on {} and no shortcut patterns matched", inserted_name);
             }
 
-            std::optional<std::string> node_script;
-            if (node["script"]) node_script = node["script"].as<std::string>();
+            // Now add the namespace to it
+            if (next != "") {
+                if (next.find('/') == std::string::npos)
+                    next = fmt::format("{}/{}", nmspace, next);
+                else
+                    // If the name is namespaced, parse the referenced file now
+                    maybe_parse_referenced_file(next, result, lua);
 
-            std::optional<std::string> node_script_after;
-            if (node["script_after"])
-                node_script_after = node["script_after"].as<std::string>();
 
+                next_references_to_check.insert({inserted_name, next});
+            }
 
-            sol::object result_object;
+            if (node["wait"] && node["wait"].as<bool>()) {
+                result_object = sol::make_object<TerminalInputWaitLineData>(lua, text, next);
+            } else {
+                result_object = sol::make_object<TerminalOutputLineData>(lua, text, next);
+            }
+        } else if (node["responses"]) {
+            decltype(TerminalVariantInputLineData::variants) variants;
 
-            if (node["text"]) {
-                // Get the text of the node
-                auto text = node["text"].as<std::string>();
+            for (auto resp : node["responses"]) {
+                auto resp_text = resp["text"].as<std::string>();
 
-                std::string next;
-                if (node["next"]) {
-                    next = node["next"].as<std::string>();
-                } else if (!node["next"] && StringUtils::is_number(name)) {
+                std::string resp_next;
+                if (!resp["next"] && StringUtils::is_number(name)) {
                     // If there's no next node, but the name of the node is a number,
                     // try using the next number as the next node
 
@@ -142,107 +185,64 @@ void StoryParser::parse(lines_type &result, std::string file_name, sol::state &l
                     auto maybe_next_name = std::to_string(next_num);
                     auto maybe_next_numbered = root_node[maybe_next_name];
 
-                    if (maybe_next_numbered)
+                    if (maybe_next_numbered) {
                         // If it exists, mark it as next
-                        next = maybe_next_name;
-                    else
-                        // Otherwise log that there was no more numbers, let "" be there
-                        spdlog::warn("Next numbered line not found for {} and there's no 'next'", inserted_name);
-                } else {
-                    // If there was no next at all and no patterns matched, let "" be there
-                    spdlog::warn("No 'next' on {} and no shortcut patterns matched", inserted_name);
-                }
-
-                // Now add the namespace to it
-                if (next != "") {
-                    if (next.find('/') == std::string::npos)
-                        next = fmt::format("{}/{}", nmspace, next);
-                    else
-                        // If the name is namespaced, parse the referenced file now
-                        maybe_parse_referenced_file(next, result, lua);
-
-
-                    next_references_to_check.insert({inserted_name, next});
-                }
-
-                if (node["wait"] && node["wait"].as<bool>()) {
-                    result_object = sol::make_object<TerminalInputWaitLineData>(lua, text, next);
-                } else {
-                    result_object = sol::make_object<TerminalOutputLineData>(lua, text, next);
-                }
-            } else if (node["responses"]) {
-                decltype(TerminalVariantInputLineData::variants) variants;
-
-                for (auto resp : node["responses"]) {
-                    auto resp_text = resp["text"].as<std::string>();
-
-                    std::string resp_next;
-                    if (!resp["next"] && StringUtils::is_number(name)) {
-                        // If there's no next node, but the name of the node is a number,
-                        // try using the next number as the next node
-
-                        auto next_num = std::stoi(name) + 1;
-                        auto maybe_next_name = std::to_string(next_num);
-                        auto maybe_next_numbered = root_node[maybe_next_name];
-
-                        if (maybe_next_numbered) {
-                            // If it exists, mark it as next
-                            resp_next = maybe_next_name;
-                        } else {
-                            spdlog::error(
-                                "Next numbered line not found for variant '{}' of {} and there's no 'next', the response needs to have somewhere to go",
-                                resp_text, inserted_name
-                            );
-                            std::terminate();
-                        }
-                    } else if (resp["next"]) {
-                        resp_next = resp["next"].as<std::string>();
+                        resp_next = maybe_next_name;
                     } else {
                         spdlog::error(
-                            "response '{}' of {} has no 'next' and the name is not a number, it needs to have somewhere to go to",
+                            "Next numbered line not found for variant '{}' of {} and there's no 'next', the response needs to have somewhere to go",
                             resp_text, inserted_name
                         );
                         std::terminate();
                     }
-
-                    // Add the namespace to next if needed
-                    if (resp_next.find('/') == std::string::npos)
-                        resp_next = fmt::format("{}/{}", nmspace, resp_next);
-                    else
-                        maybe_parse_referenced_file(resp_next, result, lua);
-
-                    next_references_to_check.insert({
-                            fmt::format("response '{}' of {}", resp_text, inserted_name),
-                            resp_next
-                    });
-
-                    // Create the variant data with the required parameters
-                    auto variant = TerminalVariantInputLineData::Variant(resp_text, resp_next);
-
-                    // If there's a condition, add it too
-                    if (resp["condition"])
-                        variant.condition = resp["condition"].as<std::string>();
-
-                    variants.push_back(variant);
+                } else if (resp["next"]) {
+                    resp_next = resp["next"].as<std::string>();
+                } else {
+                    spdlog::error(
+                        "response '{}' of {} has no 'next' and the name is not a number, it needs to have somewhere to go to",
+                        resp_text, inserted_name
+                    );
+                    std::terminate();
                 }
 
-                result_object = sol::make_object<TerminalVariantInputLineData>(lua, variants);
-            } else {
-                spdlog::error("Unknown line type at {}", inserted_name);
-                std::terminate();
+                // Add the namespace to next if needed
+                if (resp_next.find('/') == std::string::npos)
+                    resp_next = fmt::format("{}/{}", nmspace, resp_next);
+                else
+                    maybe_parse_referenced_file(resp_next, result, lua);
+
+                next_references_to_check.insert({
+                        fmt::format("response '{}' of {}", resp_text, inserted_name),
+                        resp_next
+                    });
+
+                // Create the variant data with the required parameters
+                auto variant = TerminalVariantInputLineData::Variant(resp_text, resp_next);
+
+                // If there's a condition, add it too
+                if (resp["condition"])
+                    variant.condition = resp["condition"].as<std::string>();
+
+                variants.push_back(variant);
             }
 
-            auto &general_line = result_object.as<TerminalLineData>();
-            general_line.character_config = char_config;
-            general_line.script = node_script;
-            general_line.script_after = node_script_after;
-
-            result.insert({inserted_name, result_object});
+            result_object = sol::make_object<TerminalVariantInputLineData>(lua, variants);
+        } else {
+            spdlog::error("Unknown line type at {}", inserted_name);
+            std::terminate();
         }
 
-        for (auto &[at, expected_next] : next_references_to_check) {
-            if (!result[expected_next]) {
-                spdlog::warn("{} wants {} as next, but it does not exist", at, expected_next);
-            }
+        auto &general_line = result_object.as<TerminalLineData>();
+        general_line.character_config = char_config;
+        general_line.script = node_script;
+        general_line.script_after = node_script_after;
+
+        result.insert({inserted_name, result_object});
+    }
+
+    for (auto &[at, expected_next] : next_references_to_check) {
+        if (!result[expected_next]) {
+            spdlog::warn("{} wants {} as next, but it does not exist", at, expected_next);
         }
+    }
 }
