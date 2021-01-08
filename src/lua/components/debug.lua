35713489330db5dae123cde4bdeb9181d920c150
Ekaterina Vaartis <vaartis@cock.li>
2021-01-08 23:26:42 +0300

    Use pcall properly

--- src/lua/components/debug.lua
+++ src/lua/components/debug.lua
@@ -178,14 +178,14 @@ function M.debug_menu()
             default_data,
             selected_ent_name
          )
-         local _, err = pcall(
+         local is_ok, err = pcall(
             entities_mod.run_comp_processor,
 
             selected_ent,
             processor_data,
             selected_ent_name
          )
-         if err then
+         if not is_ok then
             debug_menu_state.added_component.last_error = err
          else
             ImGui.CloseCurrentPopup()
