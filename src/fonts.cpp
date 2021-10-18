b3bff72ba16178cea1985407706ba24870d4d6df
Ekaterina Vaartis <vaartis@kotobank.ch>
2021-10-18 20:37:12 +0300

    Replace font with Abaddon for mod
    Font from https://caffinate.itch.io/abaddon

--- src/fonts.cpp
+++ src/fonts.cpp
@@ -3,7 +3,7 @@
 #include <stdexcept>
 
 StaticFonts::StaticFonts() {
-    if (!main_font.loadFromFile("resources/fonts/Ubuntu-R.ttf")) {
+    if (!main_font.loadFromFile("resources/fonts/Abaddon Bold.ttf")) {
         throw std::runtime_error("Error loading font");
     }
 }
