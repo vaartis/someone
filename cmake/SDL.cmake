d9fa8926af141af43580fc91c3ea3053558dbfb1
Ekaterina Vaartis <vaartis@kotobank.ch>
2022-06-10 22:26:32 +0300

    Migrate SDL stuff to submodules
    They all work now, even with emscripten

---
+++ cmake/SDL.cmake
@@ -0,0 +1,42 @@
+# SDL
+
+if(NOT WIN32)
+  set(SDL_JOYSTICK OFF CACHE BOOL "Disable joystick" FORCE)
+  set(SDL_HAPTIC OFF CACHE BOOL "Disable haptic" FORCE)
+  set(SDL_SENSOR OFF CACHE BOOL "Disable sensor" FORCE)
+  set(SDL_POWER OFF CACHE BOOL "Disable power" FORCE)
+  set(SDL_LOCALE OFF CACHE BOOL "Disble locale" FORCE)
+  set(SDL_WAYLAND OFF CACHE BOOL "Disble wayland" FORCE)
+endif()
+add_subdirectory(deps/SDL EXCLUDE_FROM_ALL)
+
+# Specifically override this for packages that want to link with the dynamic
+# system library instead of our static one
+set(SDL2_INCLUDE_DIR "${PROJECT_SOURCE_DIR}/deps/SDL/include" CACHE STRING "" FORCE)
+set(SDL2_LIBRARY SDL2-static CACHE STRING "" FORCE)
+
+# SDL_mixer
+
+set(BUILD_TESTING FALSE CACHE BOOL "Disble OGG tests" FORCE)
+set(SUPPORT_OGG TRUE CACHE BOOL "Disable FLAC" FORCE)
+set(SUPPORT_FLAC FALSE CACHE BOOL "Disable FLAC" FORCE)
+set(SUPPORT_OPUS FALSE CACHE BOOL "Disable opus" FORCE)
+set(SUPPORT_MP3_MPG123 FALSE CACHE BOOL "Disable MPG123" FORCE)
+set(SUPPORT_MOD_MODPLUG FALSE CACHE BOOL "Disable modplug" FORCE)
+set(SUPPORT_MID_TIMIDITY FALSE CACHE BOOL "Disble timidity" FORCE)
+add_subdirectory(deps/SDL_mixer EXCLUDE_FROM_ALL)
+
+# SDL_ttf
+
+set(BUILD_SAMPLES FALSE CACHE BOOL "Don't build samples" FORCE)
+set(TTF_DISABLE_INSTALL TRUE CACHE BOOL "Disable install" FORCE)
+add_subdirectory(deps/SDL_ttf EXCLUDE_FROM_ALL)
+
+# SDL_gpu
+
+set(DISABLE_GLES_1 TRUE CACHE BOOL "Disable GLES1" FORCE)
+set(INSTALL_LIBRARY FALSE CACHE BOOL "Disable installing" FORCE)
+set(BUILD_STATIC TRUE CACHE BOOL "Enable static" FORCE)
+set(BUILD_SHARED FALSE CACHE BOOL "Disable shared" FORCE)
+set(BUILD_DEMOS FALSE CACHE BOOL "Disable demos" FORCE)
+add_subdirectory(deps/SDL_gpu EXCLUDE_FROM_ALL)
